90a762c6bf060503a47dc89a2db0a9e31e2a4cdb
ABHISHEK KUMAR <100033618+abhishek110803@users.noreply.github.com>
2023-07-23 23:41:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Prefinal Year Cse Student At NITJ", "Full Stack Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+
+
+
+
+var acc = document.getElementsByClassName("accordionnew");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "activenew" class,
+        to highlight the button that controls the panelnew */
+        this.classList.toggle("activenew");
+
+        /* Toggle between hiding and showing the activenew panelnew */
+        var panelnew = this.nextElementSibling;
+        if (panelnew.style.display === "block") {
+            panelnew.style.display = "none";
+        } else {
+            panelnew.style.display = "block";
+            
+        }
+    });
+}
+
+
+//project
+
+
+    function openCity(evt, cityName) {
+    var i, dummycontent, project;
+    dummycontent = document.getElementsByClassName("dummycontent");
+    for (i = 0; i < dummycontent.length; i++) {
+        dummycontent[i].style.display = "none";
+    }
+    project = document.getElementsByClassName("project");
+    for (i = 0; i < project.length; i++) {
+        project[i].className = project[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
